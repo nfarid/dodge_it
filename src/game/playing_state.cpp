d009bd43ae8aaed9866846f9046ec08769c312f3
nfarid <54642193+nfarid@users.noreply.github.com>
2022-10-14 12:21:34 +0100

    Created Game::PlayingState class.

--- src/game/playing_state.cpp
+++ src/game/playing_state.cpp
@@ -1,2 +1,17 @@
 
 #include "playing_state.hpp"
+
+
+namespace Game
+{
+
+
+void PlayingState::handleInput() {}
+
+void PlayingState::update(Util::Second dt) {}
+
+void PlayingState::draw() {}
+
+
+} // namespace Game
+
--- src/game/playing_state.hpp
+++ src/game/playing_state.hpp
@@ -2,4 +2,24 @@
 #ifndef HPP_GAME_PLAYINGSTATE_
 #define HPP_GAME_PLAYINGSTATE_
 
+#include "../media/game_state.hpp"
+
+
+namespace Game
+{
+
+
+class PlayingState : public Media::GameState {
+public:
+
+    virtual void handleInput();
+    virtual void update(Util::Second dt);
+    virtual void draw();
+
+private:
+};
+
+
+}// namespace Game
+
 #endif // ifndef HPP_GAME_PLAYINGSTATE_
