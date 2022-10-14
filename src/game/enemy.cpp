8de5dbdeaa47b5a0214ed07b895e05d0349621be
nfarid <54642193+nfarid@users.noreply.github.com>
2022-10-14 21:17:08 +0100

    Created Enemy class.

--- src/game/enemy.cpp
+++ src/game/enemy.cpp
@@ -1,2 +1,44 @@
 
 #include "enemy.hpp"
+
+#include "../media/core.hpp"
+#include "../media/window.hpp"
+
+
+namespace Game
+{
+
+
+using namespace Util::Udl;
+using namespace Media::Udl;
+
+
+namespace
+{
+
+
+const Media::AnimationFrame circleFrame{
+    .rect = Media::PixelRect::leftTopSize({64_pl, 0_pl}, {64_pl, 64_pl}),
+    .time = Media::forever,
+};
+const Media::Animation circleAnimation = {
+    .modeName="default",
+    .frameLst={circleFrame},
+};
+
+
+} // namespace
+
+
+Enemy::Enemy(SDL_Texture* texture_) :
+    mCircle{.r=2_bl, .pos={10_bl, 10_bl} },
+    mSprite{ texture_, {circleAnimation} }
+{}
+
+void Enemy::draw(Media::Window& window) const {
+    window.draw(mSprite, mCircle.aabb() );
+}
+
+
+} // namespace Game
+
--- src/game/enemy.hpp
+++ src/game/enemy.hpp
@@ -2,8 +2,30 @@
 #ifndef HPP_GAME_ENEMY_
 #define HPP_GAME_ENEMY_
 
+#include "circle.hpp"
+
+#include "../media/drawable.hpp"
+#include "../media/sprite.hpp"
+
+#include <SDL2/SDL_render.h>
+
 
 namespace Game
-{}
+{
+
+
+class Enemy : public Media::Drawable {
+public:
+    explicit Enemy(SDL_Texture* texture_);
+
+private:
+    Circle mCircle{};
+    Media::Sprite mSprite;
+
+    void draw(Media::Window& window) const override;
+};
+
+
+}// namespace Game
 
 #endif // ifndef HPP_GAME_ENEMY_
