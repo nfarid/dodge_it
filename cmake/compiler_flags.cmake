e4f7c547696e79f0288abb85d9e6add6097e770c
nfarid <54642193+nfarid@users.noreply.github.com>
2022-10-21 20:00:20 +0100

    Changed how additional warnings are added.

--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,46 +1,50 @@
 
+include("CheckCXXCompilerFlag")
+
 function(add_additional_warnings TARGET_NAME)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        #Add warnings for GCC and Clang
-        target_compile_options("${TARGET_NAME}"
-            PRIVATE "-Wall"
-            PRIVATE "-Wextra"
-            PRIVATE "-Wpedantic"
-            PRIVATE "-Warray-bounds"
-            PRIVATE "-Wcast-align"
-            PRIVATE "-Wconversion"
-            PRIVATE "-Wdangling-else"
-            PRIVATE "-Wimplicit-fallthrough"
-            PRIVATE "-Wfloat-equal"
-            PRIVATE "-Winit-self"
-            PRIVATE "-Wmain"
-            PRIVATE "-Wmissing-declarations"
-            PRIVATE "-Wnon-virtual-dtor"
-            PRIVATE "-Wparentheses"
-            PRIVATE "-Wpointer-arith"
-            PRIVATE "-Wredundant-decls"
-            PRIVATE "-Wswitch"
-            PRIVATE "-Wstrict-overflow"
-            PRIVATE "-Wuninitialized"
-            PRIVATE "-Wundef"
-            PRIVATE "-Wunreachable-code"
-            PRIVATE "-Wunused"
-        )
-        #Clang has additional warnings
-        if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-            target_compile_options("${TARGET_NAME}"
-                PRIVATE "-Wcovered-switch-default"
-                PRIVATE "-Wdeprecated"
-                PRIVATE "-Wdeprecated-copy"
-                PRIVATE "-Wdeprecated-copy-dtor"
-                PRIVATE "-Wmissing-prototypes"
-                PRIVATE "-Wmissing-variable-declarations"
-                PRIVATE "-Wsuggest-destructor-override"
-                PRIVATE "-Wnewline-eof"
-                PRIVATE "-Wunused-exception-parameter"
-            )
+    set(WARNING_LIST
+        #GCC and clang warnings
+        "-Wall"
+        "-Wextra"
+        "-Wpedantic"
+        "-Warray-bounds"
+        "-Wcast-align"
+        "-Wconversion"
+        "-Wdangling-else"
+        "-Wimplicit-fallthrough"
+        "-Wfloat-equal"
+        "-Winit-self"
+        "-Wmain"
+        "-Wmissing-declarations"
+        "-Wnon-virtual-dtor"
+        "-Wparentheses"
+        "-Wpointer-arith"
+        "-Wredundant-decls"
+        "-Wswitch"
+        "-Wstrict-overflow"
+        "-Wuninitialized"
+        "-Wundef"
+        "-Wunreachable-code"
+        "-Wunused"
+        #Clang warnings
+        "-Wcovered-switch-default"
+        "-Wdeprecated"
+        "-Wdeprecated-copy"
+        "-Wdeprecated-copy-dtor"
+        "-Wmissing-prototypes"
+        "-Wmissing-variable-declarations"
+        "-Wsuggest-destructor-override"
+        "-Wnewline-eof"
+        "-Wunused-exception-parameter"
+    )
+
+    #Only add the warning if it's accepted by the compiler
+    foreach(WARNING ${WARNING_LIST})
+        check_cxx_compiler_flag("${WARNING}" ACCEPT_${WARNING})
+        if(ACCEPT_${WARNING})
+            target_compile_options("${TARGET_NAME}" PRIVATE "${WARNING}")
         endif()
-    endif()
+    endforeach()
 endfunction()
 
 
