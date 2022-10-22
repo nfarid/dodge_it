2b8930b61b80d2848b6d3428306eb8ae0cfd1e7f
nfarid <54642193+nfarid@users.noreply.github.com>
2022-10-22 23:20:29 +0100

    Created rng.

--- src/util/rng.cpp
+++ src/util/rng.cpp
@@ -1,2 +1,37 @@
 
 #include "rng.hpp"
+
+#include <chrono>
+
+
+namespace Util
+{
+
+
+namespace
+{
+
+
+// TODO: check out random_device and other random methods
+// for now just use current time
+auto getSeed() {
+    const auto seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    return static_cast<std::mt19937::result_type>(seed);
+}
+
+
+} // namespace
+
+
+Rng::Rng() :
+    mRngEngine{getSeed()}
+{}
+
+float Rng::getFloat(float minimum, float maximum) {
+    std::uniform_real_distribution distr{minimum, maximum};
+    return distr(mRngEngine);
+}
+
+
+} // namespace Util
+
--- src/util/rng.hpp
+++ src/util/rng.hpp
@@ -2,4 +2,27 @@
 #ifndef HPP_UTIL_RNG_
 #define HPP_UTIL_RNG_
 
+#include <random>
+
+
+namespace Util
+{
+
+
+/**
+ * @brief Very simple rng engine
+ */
+class Rng {
+public:
+    explicit Rng();
+
+    float getFloat(float minimum, float maximum);
+
+private:
+    std::mt19937 mRngEngine;
+};
+
+
+}// namespace Util
+
 #endif // ifndef HPP_UTIL_RNG_
